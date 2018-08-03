ba3220c430b1ebc544533421ba39fc72b581193f
toonraon <toonraon@naver.com>
2018-08-03 23:49:24 +0900

    SNS 공유 버튼 완성

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ function connectorDownload() {
     hiddenText.style.display = "block";
   }
 }
+
+function shareSNS(type) {
+  if(type === "facebook") {
+    window.open("http://www.facebook.com/sharer/sharer.php?u=toonraon.cf","_blank");
+  } else if(type === "twitter") {
+    window.open("https://twitter.com/intent/tweet?text=ToonRaon 싸지방 접속기&url=toonraon.cf","_blank");
+  } else if(type === "googleplus") {
+    window.open("https://plus.google.com/share?url=toonraon.cf","_blank");
+  } else if(type === "naverblog") {
+    window.open("http://blog.naver.com/openapi/share?url=toonraon.cf","_blank");
+  }
+}
